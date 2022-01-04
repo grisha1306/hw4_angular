fc8b2d3fed1e5329b91c0e742aa0d8c86d175a19
grigoriy.shumilov <gri1306@mail.ru>
2022-01-04 11:39:58 +0300

    Update student.component.ts

--- src/student/student.component.ts
+++ src/student/student.component.ts
@@ -1,6 +1,5 @@
 import {Component, HostListener, OnInit} from "@angular/core";
 import {Student} from "./model/student";
-import {PopupService} from "@clementvh/angular-popup";
 
 @Component({
   selector: 'app-student',
